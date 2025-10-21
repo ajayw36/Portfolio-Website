8cb1930b47d9ea25367043c4e99e636655b325ed
Ajay Wadhwani <64669240+ajayw36@users.noreply.github.com>
2025-10-21 13:26:57 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
- 
+ /** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  // 👇 Only uncomment if your GitHub Pages URL is ajayw36.github.io/Portfolio-Website/
+  basePath: '/Portfolio-Website',
+  assetPrefix: '/Portfolio-Website/',
+}
+
+module.exports = nextConfig
